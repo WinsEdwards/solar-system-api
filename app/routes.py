ad989ea9057a10f7e89e9076a3714b08510f0d14
Winslow <owedwards@gmail.com>
2023-04-25 13:30:46 -0400

    Completed def handle_single_planet()

--- app/routes.py
+++ app/routes.py
@@ -57,9 +57,3 @@ def handle_single_planet(planet_id):
             "description": planet.description,
             "moons": planet.moons
         }
-
-###As a client, I want to send a request...
-
-# 1. ...to get one existing `planet`, so that I can see the `id`, `name`, `description`, and other data of the `planet`.
-# 1. ... such that trying to get one non-existing `planet` responds with get a `404` response, so that I know the `planet` resource was not found.
-# 1. ... such that trying to get one `planet` with an invalid `planet_id` responds with get a `400` response, so that I know the `planet_id` was invalid.
